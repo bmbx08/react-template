665b768ca2ca2e3be53aa2600e88332b8056db75
bmbx08 <bmbx08@bu.ac.kr>
2024-11-07 15:36:26 +0900

    added store(redux)

--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux';
 
 const Homepage = () => {
+  // let count = useSelector((state)=>state.count);
+  // const dispatch = useDispatch();
+
+  // const increase= ()=>{
+  //   dispatch({type:"INCREMENT"})
+  // }
+
   return (
     <div>
       Homepage
+      {/* <div>
+        {count}
+        <button onClick={increase}>증가!</button>
+      </div> */}
     </div>
   )
 }
